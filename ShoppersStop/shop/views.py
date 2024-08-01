69c5402954b5b5fc3af94ebd1a97ea27c08d0e78
Meghna0121 <juvekar.meghna@gmail.com>
2024-08-02 00:20:20 +0530

    category-wise items

--- ShoppersStop/shop/views.py
+++ ShoppersStop/shop/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 # Create your views here.
 
 def home(request):
-    return render(request,"shop/home.html")
+    # products = Product.objects.all()
+    
+    # params = {'no_of_slides':nslides,'range':range(1,nslides),'product':products}
+    # allProds = [[products,range(1,nslides),nslides],
+    #             [products,range(1,nslides),nslides]]
+    allProds = []
+    catprods = Product.objects.values('category','id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nslides = n//4 + ceil((n/4)-(n//4))
+        allProds.append([prod, range(1,nslides),nslides])
+    params = {'allProds':allProds}
+    return render(request,"shop/home.html", params)
 
 def about(request):
     return render(request,"shop/about.html")
