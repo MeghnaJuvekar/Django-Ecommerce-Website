0e253c3e096bb389416eece0d2028fba643a0495
Meghna0121 <juvekar.meghna@gmail.com>
2024-08-12 14:22:52 +0530

    cart quantitty items

--- ShoppersStop/shop/urls.py
+++ ShoppersStop/shop/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('about/', views.about, name='aboutus'),
     path('contact/', views.contact, name='contactus'),
     path('tracker/', views.tracker, name='trackingstatus'),
+    path('contact/', views.contact, name='contact'),
     path('search/', views.search, name='search'),
     path('products/<int:id>', views.productview, name='productview'),
     path('checkout/', views.checkout, name='checkout'),
