ad82deea85198fd8781a828ddc6c4989766fab4e
Meghna0121 <juvekar.meghna@gmail.com>
2024-08-20 23:58:33 +0530

    Blog and Blogpost page

--- ShoppersStop/blog/urls.py
+++ ShoppersStop/blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog, name='blog'),
+    path('', views.blog, name='blogHome'),
+    path('blogpost/<int:id>/', views.blogpost, name='blogPost'),
 ]
--- ShoppersStop/blog/views.py
+++ ShoppersStop/blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import BlogPost
 
 # Create your views here.
 def blog(request):
-    return HttpResponse("This is Blog")
+    myposts = BlogPost.objects.all()
+    return render(request,"blog/index.html",{'myposts':myposts})
+
+def blogpost(request,id):
+    post = BlogPost.objects.filter(post_id = id)[0]
+    print(post)
+    return render(request,"blog/blogpost.html",{'post':post})
